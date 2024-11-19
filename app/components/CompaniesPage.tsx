4138c439679d446941e5dfd459b12265b22e8054
Talha Jamal <chrisLandson057@gmail.com>
2024-11-20 00:29:42 +0500

    Update CompaniesPage.tsx

--- app/components/CompaniesPage.tsx
+++ app/components/CompaniesPage.tsx
@@ -1,7 +1,16 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 function CompaniesPage() {
+  const logos = [
+    { src: '/Images/Logo1.png', alt: 'Logo 1' },
+    { src: '/Images/Logo2.png', alt: 'Logo 2' },
+    { src: '/Images/Logo3.png', alt: 'Logo 3' },
+    { src: '/Images/Logo4.png', alt: 'Logo 4' },
+    { src: '/Images/Logo5.png', alt: 'Logo 5' },
+    { src: '/Images/Logo6.png', alt: 'Logo 6' },
+  ];
+
   return (
     <div className="w-full py-[40px] md:py-[80px] flex flex-col md:flex-row items-center md:items-start gap-8 md:gap-16 px-4 md:px-16 mt-16">
       {/* Right Side */}
@@ -13,51 +22,19 @@ function CompaniesPage() {
 
       {/* Left Side */}
       <div className="w-full md:w-auto flex flex-wrap gap-4 justify-center md:justify-start">
-        <Image
-          src="/Images/Logo1.png"
-          alt="logo1"
-          width={123}
-          height={38}
-          className="object-contain"
-        />
-        <Image
-          src="/Images/Logo2.png"
-          alt="logo2"
-          width={123}
-          height={38}
-          className="object-contain"
-        />
-        <Image
-          src="/Images/Logo3.png"
-          alt="logo3"
-          width={123}
-          height={38}
-          className="object-contain"
-        />
-        <Image
-          src="/Images/Logo4.png"
-          alt="logo4"
-          width={123}
-          height={38}
-          className="object-contain"
-        />
-        <Image
-          src="/Images/Logo5.png"
-          alt="logo5"
-          width={123}
-          height={38}
-          className="object-contain"
-        />
-        <Image
-          src="/Images/Logo6.png"
-          alt="logo6"
-          width={123}
-          height={38}
-          className="object-contain"
-        />
+        {logos.map((logo, index) => (
+          <Image
+            key={index}
+            src={logo.src}
+            alt={logo.alt}
+            width={123}
+            height={38}
+            className="object-contain"
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default CompaniesPage
+export default CompaniesPage;

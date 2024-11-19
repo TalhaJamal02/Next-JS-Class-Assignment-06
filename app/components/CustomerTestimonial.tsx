a705473cfb8af891a07b75e2f7152531f367231d
Talha Jamal <chrisLandson057@gmail.com>
2024-11-20 00:32:41 +0500

    Update CustomerTestimonial.tsx

--- app/components/CustomerTestimonial.tsx
+++ app/components/CustomerTestimonial.tsx
@@ -4,7 +4,7 @@ import Carousel from './ui/CustomerCarousel'
 function CustomerTestimonial() {
   return (
     <div>
-      <div className=' h-[100%] pb-16 pt-16 '>
+      <div className=' h-full pb-16 pt-16 '>
         <div className=" text-center p-4 mt-16 sm:0">
           <h1 className=' font-bold text-[48px] leading-[57px]'>Customer testimonials</h1>
           <p className=' mt-6 mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
